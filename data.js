76d936e122bf17c8b67f3de9910b8e2c98f7e066
Karel Diaz <kdiazalfonso94@gmail.com>
2023-06-06 17:54:31 -0400

    adding my data

--- data.js
+++ data.js
@@ -1,338 +1,69 @@
 /* eslint-disable prettier/prettier */
 const data = {
-  name: 'Karel Díaz Alfonso',
-  phone: '+5354653411',
+  name: 'Raymond Abreu Fleita',
+  email: 'raydmonabreufleita@gmail.com',
+  phone: '+5353737330',
   socialMedias: [
     {
       name: 'Telegram',
       icon: 'telegram',
-      link: 'https://t.me/KarelDiazAlfonso',
+      link: 'https://t.me/+5353737330',
     },
-    {
-      name: 'Twitter',
-      icon: 'twitter',
-      link: 'https://twitter.com/KarelDiazA',
-    },
-    {
-      name: 'LinkedIn',
-      icon: 'linkedin',
-      link: 'https://linkedin.com/KarelDiaz',
-    },
+    // {
+    //   name: 'Twitter',
+    //   icon: 'twitter',
+    //   link: 'https://twitter.com/KarelDiazA',
+    // },
+    // {
+    //   name: 'LinkedIn',
+    //   icon: 'linkedin',
+    //   link: 'https://linkedin.com/KarelDiaz',
+    // },
     {
       name: 'GitHub',
       icon: 'github',
       link: 'https://github.com/KarelDiaz',
     },
   ],
-  skillsFrontend: [
-    { tecName: 'Vue', stars: 5 },
-    { tecName: 'Angular', stars: 4 },
-    { tecName: 'Tailwind', stars: 5 },
-  ],
-  skillsBackend: [
-    { tecName: 'Express', stars: 5 },
-    { tecName: 'Strapi', stars: 5 },
-  ],
   histories: [
     // Work
     {
       name: 'Work',
       contents: [
-        // Dibaq
-        {
-          name: 'Visor Mercantil',
-          name2: 'Undanet',
-          dateStart: '27-09-2022',
-          dateEnd: 'current',
-          description: 'Undanet data manager.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '1 Developers',
-          tasks: ['Frontend development'],
-          technologies: ['Vue'],
-          links: [],
-        },
-        // Dibaq
-        {
-          name: 'Dibaq',
-          name2: 'Dibaq',
-          dateStart: '25-09-2022',
-          dateEnd: 'current',
-          description: 'Pet food page.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '1 Developers',
-          tasks: ['Mackup'],
-          technologies: ['HTML', 'CSS', 'JavaScript'],
-          links: [],
-        },
-        // iNuba
-        {
-          name: 'iNuba Wellness',
-          name2: 'Inuba',
-          dateStart: '19-08-2022',
-          dateEnd: '27-09-2022',
-          description: 'Data analysis and visualization of personal health.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '1 Developers',
-          tasks: ['Mackup'],
-          technologies: ['React'],
-          links: [],
-        },
-        // ALD Automitive
-        {
-          name: 'ALD Automitive',
-          name2: 'ALD Automitive',
-          dateStart: '25-07-2022',
-          dateEnd: '15-08-2022',
-          description: 'A car sales manager.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '2 Developers',
-          tasks: ['Mackup'],
-          technologies: ['Vue'],
-          links: [],
-        },
-        // Alhambra filtro
-        {
-          name: 'Alhambra Filtro Fotografico',
-          name2: 'Alhambra',
-          dateStart: '20-07-2022',
-          dateEnd: '11-07-2022',
-          description: 'Ahlambra launch a new product. That came with this web to promote the product.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '2 Developers',
-          tasks: ['Mackup'],
-          technologies: ['Canvas'],
-          links: [],
-        },
-        // ETPay
-        {
-          name: 'ETPay',
-          name2: 'ETPay',
-          dateStart: '01-06-2022',
-          dateEnd: '23-07-2022',
-          description: 'Info web.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '3 Developers',
-          tasks: ['Mackup'],
-          technologies: ['React'],
-          links: [
-            {
-              title: 'You can find it here',
-              link: 'https://www.etpayment.com',
-            },
-          ],
-        },
-        // Blackjack
-        {
-          name: 'Blackjack',
-          name2: 'Informage Studio',
-          dateStart: '01-05-2022',
-          dateEnd: '30-05-2022',
-          description: 'Bet roulette.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '1 Developers',
-          tasks: ['Bet blackjack cliente.'],
-          technologies: ['Vue'],
-          links: [],
-        },
-        // KloutFood Web Corporativa
-        {
-          name: 'KloutFood Web Corporativa',
-          name2: 'Informage Studio',
-          dateStart: '01-04-2021',
-          dateEnd: '30-04-2022',
-          description: 'A web site to show info about Klout Food.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '1 Developers',
-          tasks: ['Mockup'],
-          technologies: ['Html', 'Css', 'JavaScript', 'Php'],
-          links: [],
-        },
-        // Superruleta
-        {
-          name: 'Superruleta',
-          name2: 'Informage Studio',
-          dateStart: '20-02-2022',
-          dateEnd: '06-05-2022',
-          description: 'Bet roulette.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '1 Developers',
-          tasks: ['Bet roulete cliente.'],
-          technologies: ['Vue'],
-          links: [],
-        },
-        // Ta-Tung
-        {
-          name: 'Ta-Tung',
-          name2: 'Informage Studio',
-          dateStart: '1-01-2022',
-          dateEnd: '20-02-2022',
-          description: 'The web site of a restaurant.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '1 Developers',
-          tasks: ['Mockup'],
-          technologies: ['Html', 'Css', 'JavaScript'],
-          links: [],
-        },
-        // KloutFood
-        {
-          name: 'KloutFood',
-          name2: 'Informage Studio',
-          dateStart: '15-12-2021',
-          dateEnd: '2-02-2022',
-          description: 'A web site to manage your diet.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '1 Developers',
-          tasks: ['Mockup'],
-          technologies: ['Html', 'Css', 'JavaScript', 'Php'],
-          links: [],
-        },
-        // Bolsa de Traspaso
-        {
-          name: 'Bolsa de Traspaso',
-          name2: 'Informage Studio',
-          dateStart: '30-11-2021',
-          dateEnd: '30-12-2021',
-          description: 'A web site to manage your business.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '2 Developers',
-          tasks: ['Mockup'],
-          technologies: ['Html', 'Css', 'JavaScript'],
-          links: [],
-        },
-        // Influencer Advisor
-        {
-          name: 'Influencer Advisor',
-          name2: 'Informage Studio',
-          dateStart: '01-11-2021',
-          dateEnd: '15-11-2021',
-          description: 'A web site to finde influncer for your bussinnes.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '3 Developers',
-          tasks: ['Frontend'],
-          technologies: ['Laravel', 'Vue', 'Tailwind'],
-          links: [],
-        },
-        // Colaboracion Ciudadana
-        {
-          name: 'Colaboracion Ciudadana',
-          name2: 'Sadeco',
-          dateStart: '01-10-2021',
-          dateEnd: '01-11-2021',
-          description:
-            'In the application it is possible share information about lost pets or animal with other user and get reward for that.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '3 Developers',
-          tasks: ['Mockup views'],
-          technologies: ['Angular', 'Ionic'],
-          links: [
-            {
-              title: 'Sadeco here',
-              link: 'https://www.sadeco.es/',
-            },
-          ],
-        },
-        // Caser Starbien
-        {
-          name: 'Caser Starbien',
-          name2: 'Theinit',
-          dateStart: '01-09-2021',
-          dateEnd: '01-11-2021',
-          description:
-            'Is a private project to help people how are inscribed in Caser Seguros to tracking his exercises.',
-          events: [],
-          role: 'Frontend Developer',
-          team: '4 Developers',
-          tasks: [],
-          technologies: ['Vuejs', 'Quasarjs'],
-          links: [
-            {
-              title: 'Theinit here',
-              link: 'https://www.theinit.com',
-            },
-            {
-              title: 'Caser Seguros here',
-              link: 'https://www.caser.es',
-            },
-          ],
-        },
+
         // Binary Coffee
         {
-          name: 'Binary Coffee',
-          name2: 'Open Source Projects',
+          name: 'Sitema Informatico de Generacion de Evaluaciones Integrales (SIGEI)',
+          name2: 'UCI',
           dateStart: '2019',
-          dateEnd: 'present',
-          description:
-            'Binary Coffee is an open-source project, with the idea of unifying the software developers and people who like Informatics in Cuba, in a place where they can interact and share knowledge. This idea born between some friends and currently what started as a blog is a website where people can publish their own articles and share with the community',
-          events: [],
-          role: 'Fullstack Developer',
-          team: 'The community',
-          tasks: [],
-          technologies: ['Angular', 'Angular Universal', 'Strapi', 'MongoDB'],
-          links: [
-            {
-              title: 'You can find it in github here',
-              link: 'https://github.com/orgs/dcs-community',
-            },
-            {
-              title: 'This is the oficial page of Binary Coffee',
-              link: 'https://binary-coffee.dev',
-            },
-          ],
-        },
-        // School timetable
-        {
-          name: 'School timetable',
-          name2: 'University City, Jose Antonio Echeverria',
-          dateStart: '2018',
           dateEnd: '2020',
           description:
-            'Organize the class system to the students and professors. ',
+            'Manage evaluations, grades and student titles',
           events: [],
-          role: 'Fullstack Developer',
-          team: '1 Developer',
-          tasks: [
-            'Implement the timetable module for public information ',
-            'Implement the manage view for professors',
-          ],
-          technologies: ['Vue', 'Tailwind', 'Strapi', 'MongoDB'],
+          role: 'Frontend Developer',
+          team: '',
+          tasks: [],
+          technologies: ['Vue', 'Tailwind'],
           links: [
-            {
-              title: 'You can look at the university here',
-              link: 'https://cuaje.edu.cu',
-            },
           ],
         },
         // Thesis, Graduation paper
         {
           name:
-            'Post-processing module to illustrative visualization of volumes using GPU',
+            'Mobile platform for students to learn programing',
           name2: 'Thesis, Graduation paper',
-          dateStart: '2017',
-          dateEnd: '2018',
+          dateStart: '2018',
+          dateEnd: '2019',
           description:
-            'It is a module to stylize the volumes generated in the software Viscmedic Illustration. Each effect uses an algorithm based in the Graphic Processing Unit (GPU) to achieve the execution in interactive times.',
+            "It's an interactive platform for android devices. In which tutorials are shown interactively and play.",
           events: [],
           role: 'Developer',
           team: '1 Developer',
           tasks: [
-            'Implement the plugin to integrate with Vismedic Illutration',
-            'Implement the each of the efects using GPU',
+            'Implement the android aplication',
+            'Implement the each programing language tutorial',
           ],
-          technologies: ['Qt', 'C++ (CPU)', 'GLSL (GPU)'],
+          technologies: ['Android', 'Java'],
           links: [
             {
               title: 'You can look at the university here',
@@ -349,13 +80,11 @@ const data = {
         {
           name: 'University of Information Sciences',
           name2: 'Bachelor of Computer Science',
-          dateStart: '2013',
-          dateEnd: '2018',
+          dateStart: '2014',
+          dateEnd: '2019',
           description: '',
           events: [
-            { name: 'ICPC Caribbean Finals', date: '2015/11' },
-            { name: 'ICPC Caribbean Finals', date: '2017/11' },
-            { name: 'Global Game Jam', date: '2018' },
+            // { name: 'ICPC Caribbean Finals', date: '2015/11' },
           ],
           role: '',
           team: '',
@@ -369,10 +98,10 @@ const data = {
           ],
         },
         {
-          name: 'IPVCE - Vladimir Ilich Lenin',
+          name: 'IPVCE - Ernesto Guevara',
           name2: 'Bachelor',
-          dateStart: '2009',
-          dateEnd: '2012',
+          dateStart: '20011',
+          dateEnd: '2014',
           description: '',
           events: [],
           role: '',
@@ -383,7 +112,7 @@ const data = {
             {
               title: 'You can look at here',
               link:
-                'https://www.ecured.cu/Instituto_Preuniversitario_Vocacional_de_Ciencias_Exactas_Vladimir_Ilich_Lenin',
+                'https://twitter.com/ipvceeguevaravc',
             },
           ],
         },
