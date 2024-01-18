eb82070481a1a982d3291f41b6e34b14294ac1ad
Karel Diaz <kdiazalfonso94@gmail.com>
2024-01-18 10:40:13 -0500

    skils

---
+++ src/components/Skil.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="flex flex-col">
+    <div class="flex items-center">
+      <span class="px-2 text-xl text-gray-400 w-28">{{ skil.name }}</span>
+      <div class="flex items-center gap-1">
+        <div
+          :class="['w-3 h-3 rounded-full', 
+          { 'bg-gradient-to-t from-blue-300 to-blue-400': i <= skil.stars },
+           { 'bg-gradient-to-t from-gray-200 to-gray-300': i > skil.stars }]"
+          v-for="i in 10"
+          :key="`skil-${skil.name}-star-${i}`"
+        ></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script> 
+
+export default {
+  props: { skil: Object },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,45 @@
       class="fixed flex items-center justify-center w-10 h-10 transition-all bg-white border border-transparent rounded-full shadow-lg bottom-5 right-5 hover:border-black"
       @click="imprimir()"
     >
-      <img class="w-6 h-6" src="../assets/print.png" alt />
+      <img
+        class="w-6 h-6"
+        src="../assets/print.png"
+        alt
+      />
     </button>
     <div class="h-screen overflow-auto space-y-7">
-      <div id="print" class="flex flex-col">
-        <personal-component class="sticky top-0 z-10 sm:sticky" :info="info" />
+      <div
+        id="print"
+        class="flex flex-col"
+      >
+        <personal-component
+          class="sticky top-0 z-10 sm:sticky"
+          :info="info"
+        />
         <div class="flex flex-col w-full p-1 m-auto space-y-6">
-          <history-component
+          <history
+            class="pl-3"
+            :history="{ name: 'Skils', content: '' }"
+          />
+          <div class="flex flex-col gap-3 ">
+            <skil
+              class="pl-3"
+              :skil="{ name: 'JavaScript', stars: 10 }"
+            />
+            <skil
+              class="pl-3"
+              :skil="{ name: 'Java', stars: 9 }"
+            />
+            <skil
+              class="pl-3"
+              :skil="{ name: 'C#', stars: 8 }"
+            />
+            <skil
+              class="pl-3"
+              :skil="{ name: 'C++', stars: 8 }"
+            />
+          </div>
+          <history
             class="pl-3"
             v-for="history in info.histories"
             :key="history.name"
@@ -27,13 +59,15 @@ import data from "../../data";
 // import html2pdf
 import html2pdf from "html2pdf.js";
 
-import HistoryComponent from "../components/HistoryComponent.vue";
+import History from "../components/History.vue";
 import PersonalComponent from "../components/PersonalComponent.vue";
+import Skil from "../components/Skil.vue";
 
 export default {
   components: {
-    HistoryComponent,
+    History,
     PersonalComponent,
+    Skil,
   },
   data() {
     return {
